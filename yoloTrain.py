dfd66604d6d9999c456a632f624deaf6696136eb
TerranceCheng <yongtat.cheng@gmail.com>
2024-03-19 20:15:28 +0800

    Added second training

--- yoloTrain.py
+++ yoloTrain.py
@@ -3,8 +3,8 @@
 from ultralytics import YOLO
 
 # Create model
-model = YOLO('yolov8n.pt')
+model = YOLO('yolov8s.pt')
 
 # Train model
-results = model.train(data='.\data.yaml', epochs=1, save_period=-1)
+results = model.train(data='.\data.yaml', epochs=2, save_period=-1)
 # model.export(format="onnx",opset=12)
